aef00a79ae80f79cb50485884c82efa252c0b317
Arthur VINCENT <arthur.vincent@csgroup.eu>
2025-05-28 10:45:45 +0200

    wip doc

--- lenapy/__init__.py
+++ lenapy/__init__.py
@@ -2,18 +2,18 @@ import warnings
 
 from lenapy import constants, lenapy_geo, lenapy_harmo, lenapy_time
 
-# try:
-#    from lenapy import lenapy_ocean
-# except:
-#    warnings.warn("To use lenapy_ocean, please install gsw : pip install gsw>=3.6.16")
-# import cf_xarray as cfxr
+try:
+    from lenapy import lenapy_ocean
+except:
+    warnings.warn("To use lenapy_ocean, please install gsw : pip install gsw>=3.6.16")
+import cf_xarray as cfxr
 
-# from lenapy.readers import geo_reader, ocean
-#
-# criteria = {
-#    "depth": {"name": "Depth|depth_std|LEVEL|level"},
-#    "longitude": {"name": "lon|LON|Longitude|LONGITUDE"},
-#    "latitude": {"name": "lat|LAT|Latitude|LATITUDE"},
-#    "time": {"name": "date|dates|TIME|Time"},
-# }
-# cfxr.set_options(custom_criteria=criteria)
+from lenapy.readers import geo_reader, ocean
+
+criteria = {
+    "depth": {"name": "Depth|depth_std|LEVEL|level"},
+    "longitude": {"name": "lon|LON|Longitude|LONGITUDE"},
+    "latitude": {"name": "lat|LAT|Latitude|LATITUDE"},
+    "time": {"name": "date|dates|TIME|Time"},
+}
+cfxr.set_options(custom_criteria=criteria)
